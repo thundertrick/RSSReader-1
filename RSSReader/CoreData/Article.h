66326f4fce318726e642d9a600d94bae0224cff4
The Hexagon <thehexagon.contact@gmail.com>
2015-03-18 11:06:56 +0100

    Cleaned up file structure.

---
+++ RSSReader/CoreData/Article.h
@@ -0,0 +1,25 @@
+//
+//  Article.h
+//  RSSReader
+//
+//  Created by Leopold Aschenbrenner on 04/03/15.
+//  Copyright (c) 2015 Leopold Aschenbrenner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Article : NSManagedObject
+
+@property (nonatomic, retain) NSString * author;
+@property (nonatomic, retain) NSString * content;
+@property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSString * link;
+@property (nonatomic, retain) NSNumber * read;
+@property (nonatomic, retain) NSString * source;
+@property (nonatomic, retain) NSString * summary;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSDate * updatedDate;
+
+@end
---
+++ RSSReader/CoreData/Feed.m
@@ -0,0 +1,20 @@
+//
+//  Feed.m
+//  RSSReader
+//
+//  Created by Leopold Aschenbrenner on 04/03/15.
+//  Copyright (c) 2015 Leopold Aschenbrenner. All rights reserved.
+//
+
+#import "Feed.h"
+
+
+@implementation Feed
+
+@dynamic link;
+@dynamic name;
+@dynamic summary;
+@dynamic title;
+@dynamic dateAdded;
+
+@end
